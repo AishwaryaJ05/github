e30543d3051b6b9896dd49cb47b92c1c002aa9bf
Aishwarya <aishwaryajuwlekar05@gmail.com>
2024-03-24 19:58:16 +0530

    commit add.cpp

---
+++ add.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+   
+    int num1, num2;
+   
+    cout << "Enter the first number: ";
+    cin >> num1;
+
+    cout << "Enter the second number: ";
+    cin >> num2;
+
+    int multi = num1 * num2;
+
+    cout << "The product of " << num1 << " and " << num2 << " is: " << multi << endl;
+
+    return 0;
+}
