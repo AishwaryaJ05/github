12268707975456948101524138329f23e62c8cc1
Aishwarya <aishwaryajuwlekar05@gmail.com>
2024-03-25 19:14:05 +0530

    commit multi.cpp

---
+++ multi.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+   
+    int num1, num2;
+   
+    cout << "Enter the first number: ";
+    cin >> num1;
+
+    cout << "Enter the second number: ";
+    cin >> num2;
+
+<<<<<<< HEAD
+    int sum = num1 + num2;
+
+    cout << "The sum of " << num1 << " and " << num2 << " is: " << sum << endl;
+=======
+    int multi = num1 * num2;
+
+    cout << "The product of " << num1 << " and " << num2 << " is: " << multi << endl;
+>>>>>>> master
+
+    return 0;
+}
